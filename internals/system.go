0516812b35d29983f8ea817b03d9e56948aaa947
federus1105 <federusrudi@gmail.com>
2025-10-26 02:46:17 +0700

    Feat ( system ) : features show menu

--- internals/system.go
+++ internals/system.go
@@ -1,1 +1,28 @@
 package internals
+
+import (
+	"fmt"
+)
+
+type OrderInterface interface {
+	ShowMenu()
+}
+
+type Order struct {
+	ItemName string
+	Quantity int
+	Total    float64
+}
+
+type OrderSystem struct {
+	Menu   []MenuItem
+	Orders []Order
+}
+
+func (o *OrderSystem) ShowMenu() {
+	fmt.Println("=====   DAFTAR MENU   ======")
+	for i, item := range o.Menu {
+		fmt.Printf("%d. %s (%s) - Rp %.2f\n", i+1, item.Name, item.Type, item.Price)
+	}
+	fmt.Println()
+}
