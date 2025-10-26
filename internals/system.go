66be654861ab366de984a979c340a76837fc0d89
federus1105 <federusrudi@gmail.com>
2025-10-26 10:49:57 +0700

    Feat ( system ) : function system add order

--- internals/system.go
+++ internals/system.go
@@ -6,6 +6,7 @@ import (
 
 type OrderInterface interface {
 	ShowMenu()
+	AddOrder()
 }
 
 type Order struct {
@@ -25,4 +26,24 @@ func (o *OrderSystem) ShowMenu() {
 		fmt.Printf("%d. %s (%s) - Rp %.2f\n", i+1, item.Name, item.Type, item.Price)
 	}
 	fmt.Println()
+}
+
+func (o *OrderSystem) AddOrder(itemName string, quantity int) {
+	var selected *MenuItem
+	for _, item := range o.Menu {
+		if item.Name == itemName {
+			selected = &item
+			break
+		}
+	}
+	if selected == nil {
+		panic("Masukkan nama item dengan benar!")
+	}
+	total := selected.Price * float64(quantity)
+	o.Orders = append(o.Orders, Order{
+		ItemName: itemName,
+		Quantity: quantity,
+		Total:    total,
+	})
+	fmt.Printf("Pesanan %s, dengan jumlah %d berhasil. Total : %f\n", itemName, quantity, total)
 }
