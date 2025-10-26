528157a236bef67b0a4e05b3fd9ad4b1a4c81bf5
federus1105 <federusrudi@gmail.com>
2025-10-26 10:49:13 +0700

    Feat ( main ) : features add order

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/federus1105/koda-b4-golang-weekly/internals"
@@ -20,15 +21,21 @@ func main() {
 
 	system := &internals.OrderSystem{
 		Menu: []internals.MenuItem{
-			{Name: "Ayam Geprek", Price: 22000, Type: "Makanan"},
-			{Name: "Ayam Geprek", Price: 22000, Type: "Makanan"},
-			{Name: "Ayam Geprek", Price: 22000, Type: "Makanan"},
+			{Name: "Nasi Uduk", Price: 15000, Type: "Makanan"},
+			{Name: "Ayam Bakar", Price: 25000, Type: "Makanan"},
+			{Name: "Ayam Geprek", Price: 20000, Type: "Makanan"},
+			{Name: "Americano", Price: 15000, Type: "Minuman"},
+			{Name: "Hazelnut Latte", Price: 25000, Type: "Minuman"},
+			{Name: "Sanger Expresso", Price: 20000, Type: "Minuman"},
+			{Name: "Strawberry Mojito", Price: 15000, Type: "Minuman"},
+			{Name: "Lime Lcy", Price: 25000, Type: "Minuman"},
+			{Name: "Lemon Tea", Price: 20000, Type: "Minuman"},
 		},
 	}
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		fmt.Println("===== SISTEM PEMESANAN =====\n")
+		fmt.Printf("\n===== SISTEM PEMESANAN =====\n")
 		fmt.Println("1. Lihat Menu")
 		fmt.Println("2. Tambah Pesanan")
 		fmt.Println("3. Lihat Pesanan")
@@ -42,11 +49,25 @@ func main() {
 		switch choice {
 		case "1":
 			system.ShowMenu()
+		case "2":
+			system.ShowMenu()
+			fmt.Printf("Masukkan nama item: ")
+			item, _ := reader.ReadString('\n')
+			item = strings.TrimSpace(item)
+
+			fmt.Printf("Masukkan jumlah: ")
+			qtyInput, _ := reader.ReadString('\n')
+			qtyInput = strings.TrimSpace(qtyInput)
+			qty, err := strconv.Atoi(qtyInput)
+			if err != nil {
+				panic("Jumlah tidak valid")
+			}
+			system.AddOrder(item, qty)
 		case "0":
 			fmt.Println("Terima kasih!")
 			os.Exit(0)
 		default:
-			fmt.Println("Pilihan tidak valid!\n")
+			fmt.Printf("Pilihan tidak valid!\n")
 		}
 	}
 
