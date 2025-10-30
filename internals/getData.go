633a61b8a2c701fbc5c246ed7cfda232d16e69a2
federus1105 <federusrudi@gmail.com>
2025-10-30 16:09:08 +0700

    Feat ( getData ) : function get data from use pgx pool

--- internals/getData.go
+++ internals/getData.go
@@ -1,11 +1,15 @@
 package internals
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func GetData(cacheDuration time.Duration) ([]MenuItem, error) {
@@ -13,7 +17,7 @@ func GetData(cacheDuration time.Duration) ([]MenuItem, error) {
 	cacheDir := os.TempDir()
 	cacheFile := filepath.Join(cacheDir, "data.json")
 
-	// === PERIKSA FILE CACHE ADA DAN MASIH VALID === 
+	// === PERIKSA FILE CACHE ADA DAN MASIH VALID ===
 	fmt.Println("Loadingg.......")
 	fileInfo, err := os.Stat(cacheFile)
 	if err == nil && time.Since(fileInfo.ModTime()) < cacheDuration {
@@ -45,4 +49,38 @@ func GetData(cacheDuration time.Duration) ([]MenuItem, error) {
 	}
 
 	return menu, nil
+}
+
+func InitDB() ([]MenuItem, error) {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Failed load .env", err)
+	}
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Println("Failed to connect", err)
+	}
+
+	var Menu MenuItem
+
+	err = conn.QueryRow(context.Background(),
+		"SELECT id, name, price, type FROM product",
+	).Scan(&Menu.Id, &Menu.Name, &Menu.Price, &Menu.Type)
+
+	if err != nil {
+		fmt.Println("Error map database")
+	}
+
+	rows, err := conn.Query(context.Background(),
+		"SELECT id, name, price, type FROM product",
+	)
+	if err != nil {
+		fmt.Println("failed get database")
+	}
+
+	product, err := pgx.CollectRows(rows, pgx.RowToStructByName[MenuItem])
+	if err != nil {
+		fmt.Println("failed to map")
+	}
+	return product, nil
 }
